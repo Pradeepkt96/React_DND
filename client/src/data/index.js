737fdf0898a03e98d80b95f7ceb9d3d2e1dfa9a3
Pradeepkt96 <100553419+Pradeepkt96@users.noreply.github.com>
2022-11-18 18:41:29 +0530

    First Commit
    Basic DND Trello Dashboard on React with 4 Components (Open, In Progress, In Review, Done).

---
+++ client/src/data/index.js
@@ -0,0 +1,45 @@
+const data = [{
+    id: 1,
+    icon: "⭕️",
+    status: "open",
+    title: "Application Form",
+    content: "Fill out Loan interest form"
+}, {
+    id: 2,
+    icon: "⭕️",
+    status: "open",
+    title: "Purchase present",
+    content: "Get a Birthday gift"
+}, {
+    id: 3,
+    icon: "⭕️",
+    status: "open",
+    title: "Invest in investments",
+    content: "Call the bank to ask about investments"
+}, {
+    id: 4,
+    icon: "⭕️",
+    status: "open",
+    title: "Weekly Meet",
+    content: "Plan a meeting with friends on Weekend"
+}];
+
+const statuses = [{
+    status: "open",
+    icon: "⭕️",
+    color: "#EB5A46"
+}, {
+    status: "in progress",
+    icon: "🔆️",
+    color: "#00C2E0"
+}, {
+    status: "in review",
+    icon: "📝",
+    color: "#C377E0"
+}, {
+    status: "done",
+    icon: "✅",
+    color: "#33FF64"
+}];
+
+export { data, statuses };
